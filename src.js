acf6bb7eef89bae58164f3d63cc2a673c2d4a5f8
justfrankie <zhuangfrankie@gmail.com>
2020-10-06 23:52:50 -0700

    updated variables for ease of use

--- src.js
+++ src.js
@@ -1,26 +1,21 @@
 const button = document.getElementById('signin_btn');
+const url = "https://secure-peak-05845.herokuapp.com/api/v1/sessions"
+const data = {"email": "test@test.com", "password": "password"}
 
-button.addEventListener('click', async _ => {
-    // e.preventDefault();
+button.addEventListener('click', async (event) => {
+    event.preventDefault();
   try {     
-    const response = await fetch('https://secure-peak-05845.herokuapp.com/api/v1/sessions', {
-      method: 'post',
+    const response = await fetch(url, {
+      method: 'POST',
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Content-Type": "text/plain"
+        "Access-Control-Allow-Methods": "OPTIONS, GET, POST, PUT, PATCH, DELETE",
+        "Content-Type": "Authorization"
         },
-      body: {
-        "email": "test@test.com",
-        "password": "password"
-      }
+      body: data
     });
     console.log('Completed!', response);
   } catch(err) {
     console.error(`Error: ${err}`);
   }
 });
-
-// button.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     console.log("testing")
-//   });
