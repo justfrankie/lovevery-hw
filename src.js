85902fca4a5085147b1f2b80cd865a92a84dd8e5
justfrankie <zhuangfrankie@gmail.com>
2020-10-08 10:58:43 -0700

    Mock request works

--- src.js
+++ src.js
@@ -1,7 +1,10 @@
 const button = document.getElementById('signin_btn');
-const url = "https://cors-anywhere.herokuapp.com/https://secure-peak-05845.herokuapp.com/api/v1/sessions"
-const requestBody = {email: "test@test.com", password: "password"}
+const url = "https://secure-peak-05845.herokuapp.com/api/v1/sessions"
 
+let emailInputData = document.getElementById("email_input")
+let passwordInputData = document.getElementById("password_input")
+
+let requestBody = {"email": emailInputData, "password": passwordInputData}
 const options = {
   method: 'POST',
   mode: 'cors',
@@ -14,8 +17,7 @@ const options = {
 
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  fetch(url, options)
-  .then((response) => response.json())
-  .then(data => console.log('Completed!', data))
-  .catch(err => console.error(`Error: ${err}`));
+  axios.post("https://cors-anywhere.herokuapp.com/https://secure-peak-05845.herokuapp.com/api/v1/sessions", {"email": "test@test.com", "password": "password"})
+  .then(data => console.log(data))
+  .catch(err => console.error(err))
 });
